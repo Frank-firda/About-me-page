a49702adf3781c9c999fafc9d0a72c68eaee6b4a
frank <505537@student.firda.nl>
2024-12-18 16:05:15 +0100

    yes

---
+++ index.php
@@ -0,0 +1,21 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>About me!</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="icon" href="icons/racing-icon.png"
+</head>
+<body>
+    <header><a></a>
+        <a class="header-button" id="html" href="languages/html.php"> <h1>html</h1> </a>
+        <a class="header-button" id="css" href="languages/css.php"> <h1>css</h1> </a>
+        <a class="header-button" id="homepage" href="index.php"> <h1>homepage</h1> </a>
+        <a class="header-button" id="php" href="languages/php.php.php"> <h1>php</h1> </a>
+        <a class="header-button" id="sql" href="languages/sql.php"> <h1>sql</h1> </a>
+    </header>
+    <main>
+
+    </main>
+</body>
+</html>
