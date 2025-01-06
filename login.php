ecfab0da9e27515cda00455c83025c585ddf78f4
frank <505537@student.firda.nl>
2025-01-06 15:11:47 +0100

    logosfunny

---
+++ login.php
@@ -0,0 +1,23 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Login pagina</title>
+    <link rel="icon" href="icons/lock.png">
+</head>
+<header>
+    <?php
+    $nav = array(
+        array("./html.php", "<h1>html</h1>"),
+        array("./css.php", "<h1>css</h1>"),
+        array("../login.php", "<h1>login</h1>"),
+        array("./php.php", "<h1>php</h1>"),
+        array("./sql.php", "<h1>sql</h1>"),
+    );
+    include '../nav.php';
+    ?>
+</header>
+<body>
+
+</body>
+</html>
